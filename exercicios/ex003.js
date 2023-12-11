2f59b41a91939861d1929986fb2f6b344d8cc351
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-11 19:30:13 -0300

    ex003 finalizado

---
+++ exercicios/ex003.js
@@ -0,0 +1,10 @@
+/*
+3. Declare uma variável e atribua valores para cada um dos dados:
+    *name: String;
+    *age: Number (integer);
+    *stars: Number (float);
+    *isSubscribed: Boolean;
+*/
+let dados = {nome: 'Gabriel', idade: 21, stars: 7.5, isSubsribed: true}
+
+console.log(typeof dados.nome, typeof dados.idade, typeof dados.stars, typeof dados.isSubsribed);
