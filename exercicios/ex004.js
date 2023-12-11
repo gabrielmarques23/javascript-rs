58f37b71d49b9b10da60d8345a31671340a2d447
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-11 19:56:29 -0300

    ex004 finalizado

---
+++ exercicios/ex004.js
@@ -0,0 +1,15 @@
+/*
+4. A variável student abaixo é de que tipo de dados?
+//R: Object
+
+4.1 Atribua a ela as mesmas propriedades e valores do exercício 3.
+
+4.2 Mostre no console a seguinte mensagem:
+
+<name> de idade <age> pesa <weight> kg.
+
+(Subistituir as tags pelos valores de cada propriedade do objeto)
+*/
+
+let student = {name: 'Carlos', age: 12, weight: 69.5, isSubscribed: true}
+console.log(`${student.name} tem ${student.age} anos e pesa ${student.weight}kg.`);
