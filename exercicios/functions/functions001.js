1a83f1a2c7205cdec177edc1b0deb50d97b8a4b0
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-12 16:47:25 -0300

    1º exercício de funções

---
+++ exercicios/functions/functions001.js
@@ -0,0 +1,14 @@
+//Criar um aplicativo de frases motivacionais
+
+//declarationb 
+function createPhrases() {
+
+    console.log('Nunca desista, persista!');
+    console.log('Revisão é a mãe do aprendizado.');
+    console.log('Seja bom, o mundo já está cheio de pessoas ruins.');
+
+} 
+
+createPhrases();
+
+console.log('Fim');
