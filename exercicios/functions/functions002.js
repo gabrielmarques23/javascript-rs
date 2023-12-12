674ecbb5094cdd57aaf93d68c821e48b37ba8b94
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-12 17:47:28 -0300

    2º exercício de funções

---
+++ exercicios/functions/functions002.js
@@ -0,0 +1,14 @@
+//function expression//function anonymous
+
+//parâmetros da função//paramenters
+const sum = function(number1, number2){
+let total = number1 + number2
+return total //nunca deicar de definir uma variável
+}
+
+let number1 = 20
+let number2 = 30
+
+console.log(`O número 1 é ${number1}`);
+console.log(`O número 2 é ${number2}`);
+console.log(`A soma é ${sum(number1, number2)}`); 
