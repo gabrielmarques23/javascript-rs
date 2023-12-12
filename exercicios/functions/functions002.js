875af6263c11feacc163c0d97a1e884ee53785ba
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-12 18:01:51 -0300

    Simplificação de uma função adicionada ao exercício
    Apenas adicionei um bloco de anotação explicando de uma maneira mais simples o trabalho de uma function.

--- exercicios/functions/functions002.js
+++ exercicios/functions/functions002.js
@@ -12,3 +12,20 @@ let number2 = 30
 console.log(`O número 1 é ${number1}`);
 console.log(`O número 2 é ${number2}`);
 console.log(`A soma é ${sum(number1, number2)}`); 
+
+
+/*
+//Simplificando funções!!!
+
+||Funções é um liquidificador||
+-----------------------------------------------
+function fazerSuco(fruta1, fruta2) {
+    return fruta1 + fruta2
+
+} ||É como se fosse uma caixa mágica que retorna algum valor(depende da configuração da função).
+------------------------------------------------
+const copo = fazerSuco('banana', 'abacaxi');
+------------------------------------------------
+console.log(copo);
+//Irá retornar "bananaabacaxi" no console
+*/
