262728ae1c801a49127d57a7405eaa96e0b1558e
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-14 20:03:48 -0300

    5º exercício de function
    Arrow Function

---
+++ exercicios/functions/functions005.js
@@ -0,0 +1,12 @@
+//function arrow (método mais moderno de usar uma function)
+
+const sayMyName = (name) => {
+
+    console.log(name)
+}
+
+sayMyName('Gabriel Marques');
+
+//Arrow functions são uma forma mais concisa de escrever funções, especialmente úteis para funções simples.
+
+//Temos que fazer essa forma de function na ordem e de maneira correta, pois ela pode sofrer o hoisting.
