3f1a7367581fd793fe9c2e81478fb907b5ce5b09
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-15 21:42:53 -0300

    Manipulando dados em JavaScript
    Manipulando Strings e Números

---
+++ manipulando-dados/index.js
@@ -0,0 +1,11 @@
+// Manipulando Strings e Números
+
+//Transformar String em Número e Número em String
+
+let string = '1234'
+console.log(Number(typeof string));
+
+let number = 4321
+
+console.log(String(typeof number));
+
