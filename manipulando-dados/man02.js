30a8fa4b8c48dda0d10e84cd37f62a2d8c42382a
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-15 21:49:01 -0300

    Manipulação de dados 2
    Manipulação de Strings e Números

---
+++ manipulando-dados/man02.js
@@ -0,0 +1,12 @@
+//Manipulando Strings e Números 
+
+//Contar quantos caracteres tem uma palavra e quantos dígitos tem um número
+
+let word = "paralelepipedo"
+
+console.log (word.length);
+
+let number = 123
+
+console.log (String(number).length);
+//Transformei o número em String para poder usar a propriedade "length", para aí sim poder contar quantos caracteres tinha.
