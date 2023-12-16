63be71a61368bd5ed95770a776ba6beeade1ab83
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-15 22:24:36 -0300

    Manipulação de dados
    Usando o .includes para achar palavras

---
+++ manipulando-dados/man06.js
@@ -0,0 +1,8 @@
+// Manipulando Strings
+
+// Verificar se o texto contém a palavra Amor
+
+let text = "Eu quero viver o Amor"
+
+console.log (text.includes("Amor"))
+//O .includes tem diferença entre maiúsculas e minúsculas 
