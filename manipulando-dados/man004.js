97fdb28d1ae3a55da6f3fa2b14f9b00051fcaa3c
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-15 22:09:25 -0300

    Manipulação de dados
    .toUpperCase e .toLowerCase

---
+++ manipulando-dados/man004.js
@@ -0,0 +1,8 @@
+// Manioulando Strings e Números 
+
+// Trasnforme letras minúsculas em maiúsculas. Faça o contrário disso também
+
+let word = "Eu estou gostando de estudar programação"
+
+console.log(word.toUpperCase() + ' (é tudo maiúsculo)');
+console.log(word.toLocaleLowerCase() + ' (é tudo minúsculo)')
