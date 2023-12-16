9f160d3bac9a360636eb17c1833b49d7111dbf19
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-15 22:38:33 -0300

    Manipulando dados
    Usando o Array.from() para transformar uma string em um elemento de um array

---
+++ manipulando-dados/man09.js
@@ -0,0 +1,8 @@
+// Manipulando Arrays 
+
+//Transformar uma cadeia de caracteres em elementos de um array
+
+let word = "manipulação"
+console.log(Array.from(word))
+
+//Transformando uma cadeia de caracteres (uma string), em um elemento de um array
