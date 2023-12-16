8a475a7d794e978ab52c9fabdfb1a44ce1f4167d
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-15 22:33:42 -0300

    Manipulação de dados 8
    Contando quantos elementos tem em um array criado, usando o .length

---
+++ manipulando-dados/man08.js
@@ -0,0 +1,11 @@
+// Manipulando Arrays
+
+// Contar elementos 
+
+console.log([
+    "a",
+    { type: "array" },
+    function () { return "Hello" }
+].length)
+
+//Ele contará quantos elementos temos em nossa array
