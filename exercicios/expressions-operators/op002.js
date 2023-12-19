d07f6708eddfe142423d0e4640ca205075bf0419
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-19 15:04:10 -0300

    2º Exercício de Espressões e Operadores

---
+++ exercicios/expressions-operators/op002.js
@@ -0,0 +1,22 @@
+/*
+Operadores unários 
+typeof
+delete
+*/
+//A propriedade typeof é um operador unário
+console.log(typeof "Gabriel")
+
+//Defini "person" como um Objeto
+const person = {
+name : 'Gabriel',
+age: 21
+
+}
+//Deletei uma propriedade de "person"
+delete person.age
+console.log (person);
+
+
+
+
+
