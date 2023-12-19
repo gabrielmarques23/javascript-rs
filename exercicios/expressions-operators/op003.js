6b25c5d2d4b5528ac89e569e46240219c548ec31
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-19 16:55:19 -0300

    3º exercicio de operadores

---
+++ exercicios/expressions-operators/op003.js
@@ -0,0 +1,16 @@
+//Operadores lógicos (logical operators)
+
+// 2 valores booleanos, quando verificados, resultara em verdadeiro ou falso.
+
+let pao = true
+let queijo = false 
+
+// AND(E) > &&
+console.log(pao && queijo)
+
+
+// OR(OU) > ||
+console.log(pao || queijo)
+
+// NOT(NÃO) > !   (Operador de negação)
+console.log(!queijo)
