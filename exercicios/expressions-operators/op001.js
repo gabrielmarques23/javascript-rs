1d582b43a344d18738abfd2f1892b111a0793bbd
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-19 13:09:36 -0300

    1º Exercício de Expressões e Operadores

---
+++ exercicios/expressions-operators/op001.js
@@ -0,0 +1,18 @@
+/*
+new
+
+- Left-hand-side expressions
+- Criar um novo objeto
+*/
+
+let name = new String('Gabriel')
+name.surName = "Marques"
+let age = new Number(21)
+
+//console.log(name,age)
+
+let date = new Date('2023-12-01')
+console.log(date)
+
+
+
