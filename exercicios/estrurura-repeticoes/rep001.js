234b9c994e94b621f2955cbba14726094e22acef
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-21 17:15:47 -0300

    1º Exercício de Estruturas de Repetição
    Conceito do "for"

---
+++ exercicios/estrurura-repeticoes/rep001.js
@@ -0,0 +1,16 @@
+/* Estrutura de repetição 
+>>for<<
+
+// break - Para a execução do loop
+// continue - Pula a execução do momento
+*/
+
+for (let i = 10; i > 0; i--) {
+   if (i === 5) {
+      continue;
+   }
+   console.log(i)
+}
+
+
+//é uma ferramenta poderosa para controlar fluxos de repetição em JavaScript.
