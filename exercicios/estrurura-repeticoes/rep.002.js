f1e9f725803a92e6b8951aba063d0465c5dca85f
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-21 17:51:24 -0300

    2º Exercício de Estruras de Repetições
    Conceito do "while"

---
+++ exercicios/estrurura-repeticoes/rep.002.js
@@ -0,0 +1,24 @@
+/* Estrutura de repetição 
+>>while<<
+*/
+
+
+let i = 0
+while(i <= 10){
+    console.log(i)
+    
+    i++;
+}
+
+
+/*Faz mais sentido usarmos o "while" quando não sabemos o momento de parada.
+
+exemplo:
+
+let i = 124075121
+while(i > 12){
+    console.log(i)
+    
+    i /= 40
+}
+*/
