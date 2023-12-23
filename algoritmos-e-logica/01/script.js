66e329b4696036aee6ac094a0ee9924f7625f04c
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-23 17:41:35 -0300

    Inicio de algorítmos e lógica de programação
    Desadio 0'

---
+++ algoritmos-e-logica/01/script.js
@@ -0,0 +1,10 @@
+/*
+Encontre a solução ao problema:
+
+Pergunte o nome do usuário e escrev a mensagem:
+"Olá [nome do usuário]"
+*/
+
+let name = window.prompt('Qual o seu nome?');
+
+alert(`Olá ${name}, que bom que está aqui!`)
