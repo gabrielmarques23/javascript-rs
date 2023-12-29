6600feeb70dd534d45db594f99f85b5472416ed0
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-28 21:50:31 -0300

    4º exercício de Algoritmos e lógica de programaão

---
+++ algoritmos-e-logica/04/notes.md
@@ -0,0 +1,7 @@
+# Fluxos de aplicação
+
+- Caminhos lógicos baseados em algum dado ou informação
+
+[] Tipo de dado: Boolean
+[] if/else
+[] Operadores comparativos
---
+++ algoritmos-e-logica/04/script.js
@@ -0,0 +1,34 @@
+/*
+Solicitar o nome do aluno e as 3 notas de bimestre, calcular a média daquele aluno.
+
+-A média positiva deverá ser maior que 6
+
+Se o aluno passou no bimestre, dar os  parabéns.
+
+Se o aluno não passou no bimestre,  motivar o aluno a dar seu melhor na prova de recuperação.
+
+Em ambos os casos, mostre uma mensagem com o nome do aluno e a nota 
+ */
+
+let nome = String(window.prompt('Olá, sejá bem vindo! Qual o seu nome?'));
+
+
+let n1 = Number.parseInt(window.prompt('Qual a sua nota do 1º bimestre?'))
+let n2 = Number.parseInt(window.prompt('Qual a sua nota do 2º bimestre?'))
+let n3 = Number.parseInt(window.prompt('Qual a sua nota do 3º bimestre?'))
+
+
+let media = Number(n1 + n2 + n3) / 3
+
+if (nome.length === 0 || ! isNaN(nome)) {
+    alert('[ERRO] Você não preencheu os dados corretamente, tente novamente.')
+} else if (media > 10) {
+    alert('Desculpe. Dectamos um erro, insira os dados novamente!')
+} else if (media == 8 || media > 8) {
+    window.alert(`Parabéns ${nome}, sua média foi: ${media.toFixed(1)}, e você está aprovadíssimo!`)
+} else if (media >= 6) {
+    window.alert(`Isso aí ${nome}, sua média é: ${media.toFixed(1)}, e você está aprovado!`)
+} else {
+    window.alert(`Poxa ${nome}, sua média foi: ${media.toFixed(1)}, e você está reprovado. Tente ir melhor na prova de recuperação`)
+}
+
