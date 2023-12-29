b69b01bab8b38fb92ca0e6d04a535544e2538c14
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-29 17:10:18 -0300

    5º Exercício de Algorítmos e lógica de programação

---
+++ algoritmos-e-logica/05/script.js
@@ -0,0 +1,43 @@
+
+/*
+Capture 10 itens para compor a lista de um supermercado.
+
+Após capturar os 10 itens, imprima-os, separando por vírgula.
+
+*/
+
+let items = []
+
+for (let item = 0; item < 10; item++) {
+itemsName = window.prompt(`Quais os itens que deseja?`)
+items[item] = itemsName
+}
+
+alert (items)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let items = []
+
+
+for (let item = 0; item < 10; item++) {
+    let itemName = window.prompt(`Digite o item número que deseja`)
+
+    items[item] = itemName
+}
+
+alert(items)
+*/
